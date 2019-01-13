caa1835fcde1069fba1f11144bf8b98a8a0552ba
zhangsan <zhangsan@itcast.cn>
2019-01-13 20:27:16 +0800

    添加一个hello类。

--- git-test-project/src/main/java/com/itheima/git/FirstClass.java
+++ git-test-project/src/main/java/com/itheima/git/FirstClass.java
@@ -1,17 +1,32 @@
 package com.itheima.git;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
+        aa();
         // hello test
         System.out.println("how are you");
+        List<String> list = new ArrayList<String>();
+        list.add("马尔扎花");
+        list.add("了解顾");
+        for (String s : list) {
+            System.out.println(s);
+        }
     }
+
+    private static void aa() {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+
     public void test() {
         int i = 0;
         i += 10;
@@ -19,5 +34,11 @@ public class FirstClass {
     }
     public void care() {
         System.out.println("你是谁？");
+        System.out.println("你是谁？");
     }
+
+
+
+
+
 }
